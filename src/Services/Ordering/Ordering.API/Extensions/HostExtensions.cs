095a870abe9834e2da49d636c76e95915ac140c9
Khaled Yousef <Khaled.Yousef@ContactCarsM365.onmicrosoft.com>
2022-08-16 14:00:42 +0200

    ordering api

---
+++ src/Services/Ordering/Ordering.API/Extensions/HostExtensions.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Polly;
+
+namespace Odering.API.Extensions
+{
+    public static class HostExtensions
+    {
+        public static IServiceCollection MigrateDatabase<TContext>(this IServiceCollection serviceCollection, Action<TContext, IServiceProvider> seeder) where TContext : DbContext
+        {
+            var provider=serviceCollection.BuildServiceProvider();
+            using (var scope = provider.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var logger = services.GetRequiredService<ILogger>();
+                var context = services.GetRequiredService<TContext>();
+
+                try
+                {
+                    logger.LogInformation("Migrating database associated with context { DbContextName}", typeof(TContext).Name);
+
+                    var retry = Policy
+                        .Handle<SqlException>()
+                        .WaitAndRetry(retryCount: 5,
+                                      sleepDurationProvider: re => TimeSpan.FromSeconds(Math.Pow(2, re)),
+                                      onRetry: (exception, retryCount, context) =>
+                                      {
+                                          logger.LogError($"Retry {retryCount} of {context.PolicyKey} at {context.OperationKey}, due to: {exception}.");
+                                      });
+
+                    retry.Execute(() => InvokeSeeder(seeder, context, services));
+                    logger.LogInformation("Migrated database associated with context {DbContextName}", typeof(TContext).Name);
+                }
+                catch (SqlException ex)
+                {
+
+                    logger.LogError(ex, "An error occurred while migrating the database used on context {DbContextName}", typeof(TContext).Name);
+                }
+            }
+            return serviceCollection;
+        }
+
+        private static void InvokeSeeder<TContext>(Action<TContext, IServiceProvider> seeder, TContext context, IServiceProvider services) where TContext : DbContext
+        {
+            context.Database.Migrate();
+            seeder(context, services);
+        }
+    }
+}
---
+++ src/Services/Ordering/Ordering.Infrastructure/Persistence/OrderContextSeed.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Logging;
+using Ordering.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Infrastructure.Persistence
+{
+    public class OrderContextSeed
+    {
+        public static async Task SeedAsync(OrderContext context,ILogger<OrderContextSeed> logger){
+            if (!context.Orders.Any())
+            {
+                context.AddRange(GetPreConfigurationOrders());
+                await context.SaveChangesAsync();
+                logger.LogInformation("seed database associated with context {DbContextName}",typeof(OrderContext).Name);
+            }
+
+        }
+
+        private static IEnumerable<Order> GetPreConfigurationOrders()
+        {
+            return new List<Order>()
+            {
+                new Order(){UserName="khaled",FirstName="Tared",LastName="ahmed",EmailAddress="",AddressLine="",Country="",TotalPrice=350 }
+            };
+        }
+    }
+}
